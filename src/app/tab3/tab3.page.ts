cb832647de556a31df0a9a3b818f688fd481166a
hjakub <j_hrabec@utb.cz>
2025-01-23 14:13:05 +0100

    App build for android, minor changes

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -9,6 +9,7 @@ import { AppStorageService } from '../app-storage.service';
 })
 export class Tab3Page {
   selectedUnit: string = 'celsius';
+  paletteToggle = false;
 
   constructor(private appStorage: AppStorageService) { }
 
@@ -17,6 +18,32 @@ export class Tab3Page {
     if (savedUnit) {
       this.selectedUnit = savedUnit;
     }
+  }
+
+  ngOnInit() {
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Initialize the dark palette based on the initial
+    // value of the prefers-color-scheme media query
+    this.initializeDarkPalette(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkPalette(mediaQuery.matches));
+  }
+
+
+  initializeDarkPalette(isDark: boolean) {
+    this.paletteToggle = isDark;
+    this.toggleDarkPalette(isDark);
+  }
+
+  toggleChange(ev: any) {
+    this.toggleDarkPalette(ev.detail.checked);
+  }
+
+  toggleDarkPalette(shouldAdd: boolean) {
+    document.documentElement.classList.toggle('ion-palette-dark', shouldAdd);
   }
 
   async onUnitChange() {
