afba3b936a92611f2243cbfc5ebfa400f3079885
hjakub <j_hrabec@utb.cz>
2025-01-23 22:12:11 +0100

    SimpleWeather

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.weather.app',
-  appName: 'AP5PM_project',
+  appName: 'SimpleWeather',
   webDir: 'www'
 };
 
